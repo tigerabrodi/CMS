64e62195d927274c7b8b867d9d634083602d1ca7
tigerabrodi <fahdabrodi@gmail.com>
2019-06-30 21:25:01 +0200

    trying to figure everyth out with heroku deployment

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-require('dotenv').config({
-    path: "node.env"
-});
+require('dotenv').config()
 const path = require('path');
+const fs = require("fs");
 const express = require('express');
-const debug = require("debug")("toto");
+const https = require("https");
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const flash = require("connect-flash");
 const passport = require("passport");
-
+const helmet = require("helmet");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
 const errorController = require('./controllers/error');
+const compression = require("compression");
+const morgan = require("morgan");
 
-const mongodb_uri = process.env.MONGODB_URI;
+const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-qb6i7.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`;
 const app = express();
 const csrf = require("csurf");
 const {
@@ -27,7 +28,7 @@ require("./config/passport")(passport);
 
 // session store
 const store = new MongoDBStore({
-    uri: mongodb_uri,
+    uri: MONGODB_URI,
     collection: "sessions"
 });
 
@@ -38,10 +39,18 @@ const csrfProtection = csrf();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+
+
 //  require routes
 const adminRoutes = require("./routes/admin");
 const blogRoutes = require("./routes/blog");
 const authRoutes = require("./routes/auth");
+
+const accessLogStream = fs.createWriteStream(
+    path.join(__dirname, 'access.log'), {
+        flags: 'a'
+    }
+);
 
 
 app.use(bodyParser.urlencoded({
@@ -54,7 +63,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // session 
 app.use(
     session({
-        secret: process.env.SECRET,
+        secret: "my secret",
         cookie: {
             maxAge: 1000 * 60 * 60 * 24 * 7
         },
@@ -71,6 +80,11 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 app.use(csrfProtection);
+app.use(helmet());
+app.use(compression());
+app.use(morgan('combined', {
+    stream: accessLogStream
+}));
 
 // global variables
 app.use((req, res, next) => {
@@ -91,13 +105,19 @@ app.use(errorController.get404);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
 
+console.log(`${process.env.MONGO_PASSWORD}`);
+console.log(`${process.env.MONGO_USER}`);
+
 
 // connection to mongodb
-mongoose.connect(mongodb_uri, {
-    useNewUrlParser: true
-});
-
-
-app.listen(process.env.PORT || 3000, () => {
-    console.log("listening on port 3000");
-})
+mongoose.connect(MONGODB_URI, {
+        useNewUrlParser: true
+    })
+    .then(result => {
+        app.listen(process.env.PORT || 3000, () => {
+            console.log("listening on port 3000");
+        })
+    })
+    .catch(err => {
+        console.log(err);
+    })
