b93ff024ae077da4f6878da9de659e31917a7434
tigerabrodi <fahdabrodi@gmail.com>
2019-06-30 21:37:33 +0200

    dawg

---
+++ models/model.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose"),
+    Schema = mongoose.Schema,
+    bcrypt = require("bcryptjs");
+
+
+
+
+const postSchema = new Schema({
+    title: String,
+    description: String,
+    context: String,
+    author: {
+        type: Schema.Types.ObjectId,
+    }
+});
+
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    posts: [postSchema]
+});
+
+
+userSchema.pre("save", async function save(next) {
+    const user = this;
+    if (!user.isModified("password")) return next();
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+    user.password = hashedPassword;
+    next();
+});
+
+
+const Post = mongoose.model("Post", postSchema);
+const User = mongoose.model("User", userSchema);
+
+module.exports = {
+    User,
+    Post
+}
